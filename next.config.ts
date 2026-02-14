ac38e0efba4da4234f788733182e163fcf8e7ec1
Russel Yasol <russel.yasol@wvsu.edu.ph>
2026-02-14 12:02:50 +0800

    feat: add rewrites configuration for CGI-bin routing

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export", 
+    async rewrites() {
+    return [
+      {
+        source: '/cgi-bin/:path*',
+        destination: 'http://192.168.224.1/cgi-bin/:path*',
+        basePath: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
