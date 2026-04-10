6f5505a5bd1043630a61bc2b72324b4471336b2e
Jeffery2008 <3510862791@qq.com>
2026-04-11 00:47:34 +0800

    chore: bump version to v0.1.5

--- scripts/install_rm520n.sh
+++ scripts/install_rm520n.sh
@@ -42,7 +42,7 @@ set -e
 
 # --- Configuration -----------------------------------------------------------
 
-VERSION="v0.1.4"
+VERSION="v0.1.5"
 INSTALL_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 # Destinations
